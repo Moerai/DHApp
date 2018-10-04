71cb38ea4295161be7b53a761710ffe4747f16d0
ToToKr <skyinbss@gmail.com>
2018-10-04 20:32:46 +0900

    버튼에 setText를 하여 Hello로 버튼 텍스트를 변경

--- DHApp/app/src/main/java/com/example/frien/dhapp/MainActivity.java
+++ DHApp/app/src/main/java/com/example/frien/dhapp/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.frien.dhapp;
 
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.frien.dhapp.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        binding.btn.setText("hello");
+
     }
 }
