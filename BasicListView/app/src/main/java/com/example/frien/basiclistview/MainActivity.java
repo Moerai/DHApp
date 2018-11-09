09097885df67c85822b3766ff414e3abb3ed0b49
ToToKr <skyinbss@gmail.com>
2018-11-09 11:59:56 +0900

    리스트뷰에 text 2개 담아서  출력

--- BasicListView/app/src/main/java/com/example/frien/basiclistview/MainActivity.java
+++ BasicListView/app/src/main/java/com/example/frien/basiclistview/MainActivity.java
@@ -4,20 +4,37 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
-
-    String [] data = {"A","B","C","D"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        ListView list = (ListView)findViewById(R.id.listview);
-        ArrayAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, data);
-        list.setAdapter(adapter);
+        ListView lv = (ListView)findViewById(R.id.listview);
 
+        ArrayList<HashMap<String, String>> mlist = new ArrayList<HashMap<String,String>>();
+
+        HashMap<String, String> map = new HashMap<String, String>();
+        map.put("item1", "kwanhee");
+        map.put("item2", "32");
+        mlist.add(map);
+
+        map = new HashMap<String, String>();
+        map.put("item1","donggun");
+        map.put("item2","24");
+        mlist.add(map);
+        map = new HashMap<String, String>();
+        map.put("item1","jungsuk");
+        map.put("item2","55");
+        mlist.add(map);
+
+        SimpleAdapter sap = new SimpleAdapter(this, mlist, android.R.layout.simple_list_item_2, new String[]{"item1","item2"}, new int[]{android.R.id.text1, android.R.id.text2});
+        lv.setAdapter(sap);
     }
 }
